9c2b47ff82865f880603b5b5e186b891a18a62b4
Diego <dromeros@uoc.edu>
2021-05-06 21:00:08 +0200

    Push Test
    Push test

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Comment test to push
 public class PlayerMovement : MonoBehaviour
 {
     public float runSpeed = 1;
