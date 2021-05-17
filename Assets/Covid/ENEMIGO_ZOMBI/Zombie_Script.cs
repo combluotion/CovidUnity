e89eaf3b3b1aa63520aba4434fb6fbc883f806e5
agonzalezgarcia11 <agonzalezgarcia11@uoc.edu>
2021-05-18 01:01:35 +0200

    .

---
+++ Assets/Covid/ENEMIGO_ZOMBI/Zombie_Script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_Script : MonoBehaviour
+{
+    public GameObject MainCharacter;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = MainCharacter.transform.position - transform.position;
+        if (direction.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        float distance = Mathf.Abs(MainCharacter.transform.position.x - transform.position.x);
+        if (distance < 1.0f)
+        {
+            animator.SetBool("Cerca", true);
+        }
+    }
+}
