5d8646b6f3e62479ed139f9c94f802e11ceb8848
Diego <dromeros@uoc.edu>
2021-05-24 14:30:13 +0200

    Achievements, Creditos

---
+++ Assets/Achievement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement : MonoBehaviour
+{
+
+public enum AchievementTypes {killBoss, nivelUno, nivelDos, nivelTres};
+[SerializeField] private AchievementTypes achievementType;
+ public AchievementTypes _achievementType{get {return achievementType;}}
+private Text text;
+public bool isUnlocked {get; private set;}
+
+private void Awake(){
+text = GetComponent<Text>();
+CheckIfAchievementIsUnlocked();
+}
+
+public void CheckIfAchievementIsUnlocked(){
+
+    if(PlayerPrefs.GetInt(achievementType.ToString()) == 0)
+    {
+        text.color = Color.red;
+    }
+    else{
+        text.color = Color.green;
+        isUnlocked = true;
+    }
+
+}
+
+public void UnlockThisAchievement()
+{
+    PlayerPrefs.SetInt(achievementType.ToString(),1);
+    Awake();
+}
+
+
+}
